05f8c9e1a9e2394878710513df817fcab80cfc10
urban <urban@dynamix.com.br>
2021-02-03 20:57:12 -0300

    adding spring cloud gateway

---
+++ api-gateway/src/test/java/urban/sandbox/springcloud/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package urban.sandbox.springcloud.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- currency-conversion-service/src/main/java/urban/sandbox/springcloud/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/urban/sandbox/springcloud/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,8 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
+// Instância fixa
 //@FeignClient(name="currency-exchange", url="localhost:8000")
+
+// Client side load balancing embutido no eureka client (versões anteriores era o ribbon)
+// Sem definir a url, o serviço irá conversar com Eureka para obter a instância por meio de um balanceador de carga
 @FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 	
